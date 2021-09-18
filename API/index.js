5a94b91d7e0b8747a34fbfd73a76e398ba815cad
Bruno <brunorodri2015@gmail.com>
2021-09-17 22:20:01 -0300

    backend 100% complete

--- API/index.js
+++ API/index.js
@@ -25,7 +25,7 @@ app.use('/categories', routeProducts);
 app.use('/devices', routeRequests);
 
 app.get('/', (req, res) => {
-    return res.json({ message: 'Server online' });
+    return res.json({ message: 'Server is online' });
 });
 
 app.use((req, res, next) => {
